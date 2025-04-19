bc1906cd7cc89004a4d35dd081e03a7079f24222
Will Bickford <wbic16@gmail.com>
2025-04-18 22:56:40 -0500

    Remove pointless phexts

--- prep.sh
+++
@@ -1,4 +0,0 @@
-FILES="alpha beta gamma delta epsilon zeta eta theta iota kappa lambda mu nu xi omicron pi rho sigma tau upsilon phi chi psi omega a b c d e f g h i j k l m n o p q r s t u v w x y z exocortex phext"
-for F in $FILES; do
-  if [ ! -f $F.phext ]; then touch $F.phext; fi
-done
